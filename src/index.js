bbc9a0cbbb3c1224c5d93e7ba65959677084534b
Mark Mead <itsmarkmead@gmail.com>
2023-07-27 09:44:04 +0100

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,27 +1,31 @@
 export default function (Alpine) {
   class ComponentWrapper extends HTMLElement {
     connectedCallback() {
-      let shadow = this.attachShadow({ mode: 'open' })
+      const shadow = this.attachShadow({ mode: 'open' })
+
+      if (this.hasAttribute(':template')) {
+        Alpine.initTree(this)
+      }
 
       if (this.attributes.template) {
-        let template = document.getElementById(this.attributes.template.value)
-        let component = new DOMParser().parseFromString(
+        const template = document.getElementById(this.attributes.template.value)
+        const component = new DOMParser().parseFromString(
           template.innerHTML,
           'text/html'
         ).body.firstChild
 
         shadow.appendChild(component)
 
-        document.addEventListener('alpine:initialized', () =>
+        document.addEventListener('alpine:initialized', () => {
           Alpine.initTree(shadow)
-        )
+        })
       }
 
       if (this.attributes.url) {
         fetch(this.attributes.url.value)
           .then((response) => response.text())
           .then((template) => {
-            let component = new DOMParser().parseFromString(
+            const component = new DOMParser().parseFromString(
               template,
               'text/html'
             ).body.firstChild
@@ -34,11 +38,11 @@ export default function (Alpine) {
     }
   }
 
-  if (window.customElements.get('x-component-wrapper')) return
+  if (window.customElements.get('x-component-wrapper')) {
+    return
+  }
 
   customElements.define('x-component-wrapper', ComponentWrapper)
 
-  Alpine.directive('component', () => {
-    new ComponentWrapper()
-  })
+  Alpine.directive('component', () => new ComponentWrapper())
 }
